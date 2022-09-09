bfbfbdcb4233ee1c49d8f9042dce93e50fa3a025
RJM <rjmbowie@gmail.com>
2022-09-09 16:57:50 +1000

    Make redundant exports private; fix abbreviation casing

--- client.go
+++ client.go
@@ -28,16 +28,12 @@ type Type interface {
 	Type() string
 }
 
-type AuthorizeReq struct {
-	ActorType    string `json:"actor_type"`
-	ActorId      string `json:"actor_id"`
-	Action       string `json:"action"`
+type Role struct {
+	ResourceID   string `json:"resource_id"`
 	ResourceType string `json:"resource_type"`
-	ResourceId   string `json:"resource_id"`
-}
-
-type AuthorizeRes struct {
-	allowed bool
+	Role         string `json:"role"`
+	ActorID      string `json:"actor_id"`
+	ActorType    string `json:"actor_type"`
 }
 
 func (c Client) apiCall(method string, path string, body io.Reader) (*http.Request, error) {
@@ -81,110 +77,115 @@ func (c Client) delete(path string, body io.Reader) (*http.Response, error) {
 }
 
 func (c Client) Authorize(actor Instance, action string, resource Instance) (bool, error) {
-	payload := AuthorizeReq{
+	payload := struct {
+		ActorType    string `json:"actor_type"`
+		ActorID      string `json:"actor_id"`
+		Action       string `json:"action"`
+		ResourceType string `json:"resource_type"`
+		ResourceID   string `json:"resource_id"`
+	}{
 		ActorType:    actor.Type(),
-		ActorId:      actor.Id(),
+		ActorID:      actor.Id(),
 		Action:       action,
 		ResourceType: resource.Type(),
-		ResourceId:   resource.Id(),
+		ResourceID:   resource.Id(),
 	}
 
-	reqBodyJson, e := json.Marshal(payload)
+	reqBodyJSON, e := json.Marshal(payload)
 	if e != nil {
 		return false, e
 	}
 
-	reqBody := bytes.NewBuffer(reqBodyJson)
+	reqBody := bytes.NewBuffer(reqBodyJSON)
 	res, e := c.post("/authorize", reqBody)
 	if e != nil {
 		return false, e
 	}
 	defer res.Body.Close()
 
-	resBodyJson, e := ioutil.ReadAll(res.Body)
+	resBodyJSON, e := ioutil.ReadAll(res.Body)
 	if e != nil {
 		return false, e
 	}
 	if res.StatusCode != 200 {
-		return false, errors.New(string(resBodyJson))
+		return false, errors.New(string(resBodyJSON))
 	}
 
-	var resBody AuthorizeRes
-	e = json.Unmarshal(resBodyJson, &resBody)
+	var resBody struct {
+		Allowed bool `json:"allowed"`
+	}
+	e = json.Unmarshal(resBodyJSON, &resBody)
 	if e != nil {
 		return false, e
 	}
-	return resBody.allowed, nil
-}
-
-type ListReq struct {
-	ActorType    string `json:"actor_type"`
-	ActorId      string `json:"actor_id"`
-	Action       string `json:"action"`
-	ResourceType string `json:"resource_type"`
-}
-
-type ListRes struct {
-	results []int
+	return resBody.Allowed, nil
 }
 
 func (c Client) List(actor Instance, action string, resource Type) ([]int, error) {
-	payload := ListReq{
+	payload := struct {
+		ActorType    string `json:"actor_type"`
+		ActorID      string `json:"actor_id"`
+		Action       string `json:"action"`
+		ResourceType string `json:"resource_type"`
+	}{
 		ActorType:    actor.Type(),
-		ActorId:      actor.Id(),
+		ActorID:      actor.Id(),
 		Action:       action,
 		ResourceType: resource.Type(),
 	}
 
-	reqBodyJson, e := json.Marshal(payload)
+	reqBodyJSON, e := json.Marshal(payload)
 	if e != nil {
 		return nil, e
 	}
 
-	reqBody := bytes.NewBuffer(reqBodyJson)
+	reqBody := bytes.NewBuffer(reqBodyJSON)
 	res, e := c.post("/list", reqBody)
 	if e != nil {
 		return nil, e
 	}
 	defer res.Body.Close()
 
-	resBodyJson, e := ioutil.ReadAll(res.Body)
+	resBodyJSON, e := ioutil.ReadAll(res.Body)
 	if e != nil {
 		return nil, e
 	}
 	if res.StatusCode != 200 {
-		return nil, errors.New(string(resBodyJson))
+		return nil, errors.New(string(resBodyJSON))
 	}
 
+	type ListRes struct {
+		Results []int `json:"results"`
+	}
 	var resBody ListRes
-	e = json.Unmarshal(resBodyJson, &resBody)
+	e = json.Unmarshal(resBodyJSON, &resBody)
 	if e != nil {
 		return nil, e
 	}
-	return resBody.results, nil
+	return resBody.Results, nil
 }
 
-type RelationReq struct {
-	FromId   string `json:"from_id"`
+type relationReq struct {
+	FromID   string `json:"from_id"`
 	FromType string `json:"from_type"`
 	Relation string `json:"relation"`
-	ToId     string `json:"to_id"`
+	ToID     string `json:"to_id"`
 	ToType   string `json:"to_type"`
 }
 
 func (c Client) AddRelation(from Instance, name string, to Instance) error {
-	reqBody := RelationReq{
-		FromId:   from.Id(),
+	reqBody := relationReq{
+		FromID:   from.Id(),
 		FromType: from.Type(),
 		Relation: name,
-		ToId:     to.Id(),
+		ToID:     to.Id(),
 		ToType:   to.Type(),
 	}
-	reqBodyJson, e := json.Marshal(reqBody)
+	reqBodyJSON, e := json.Marshal(reqBody)
 	if e != nil {
 		return e
 	}
-	reqBodyBytes := bytes.NewBuffer(reqBodyJson)
+	reqBodyBytes := bytes.NewBuffer(reqBodyJSON)
 	res, e := c.post("/relations", reqBodyBytes)
 	if e != nil {
 		return e
@@ -201,18 +202,18 @@ func (c Client) AddRelation(from Instance, name string, to Instance) error {
 }
 
 func (c Client) DeleteRelation(from Instance, name string, to Instance) error {
-	reqBody := RelationReq{
-		FromId:   from.Id(),
+	reqBody := relationReq{
+		FromID:   from.Id(),
 		FromType: from.Type(),
 		Relation: name,
-		ToId:     to.Id(),
+		ToID:     to.Id(),
 		ToType:   to.Type(),
 	}
-	reqBodyJson, e := json.Marshal(reqBody)
+	reqBodyJSON, e := json.Marshal(reqBody)
 	if e != nil {
 		return e
 	}
-	reqBodyBytes := bytes.NewBuffer(reqBodyJson)
+	reqBodyBytes := bytes.NewBuffer(reqBodyJSON)
 	res, e := c.delete("/relations", reqBodyBytes)
 	if e != nil {
 		return e
@@ -228,27 +229,19 @@ func (c Client) DeleteRelation(from Instance, name string, to Instance) error {
 	return nil
 }
 
-type Role struct {
-	ResourceId   string `json:"resource_id"`
-	ResourceType string `json:"resource_type"`
-	Role         string `json:"role"`
-	ActorId      string `json:"actor_id"`
-	ActorType    string `json:"actor_type"`
-}
-
 func (c Client) AddRole(actor Instance, name string, resource Instance) error {
 	reqBody := Role{
-		ActorId:      actor.Id(),
+		ActorID:      actor.Id(),
 		ActorType:    actor.Type(),
 		Role:         name,
-		ResourceId:   resource.Id(),
+		ResourceID:   resource.Id(),
 		ResourceType: resource.Type(),
 	}
-	reqBodyJson, e := json.Marshal(reqBody)
+	reqBodyJSON, e := json.Marshal(reqBody)
 	if e != nil {
 		return e
 	}
-	reqBodyBytes := bytes.NewBuffer(reqBodyJson)
+	reqBodyBytes := bytes.NewBuffer(reqBodyJSON)
 	res, e := c.post("/roles", reqBodyBytes)
 	if e != nil {
 		return e
@@ -266,18 +259,21 @@ func (c Client) AddRole(actor Instance, name string, resource Instance) error {
 
 func (c Client) DeleteRole(actor Instance, name string, resource Instance) error {
 	reqBody := Role{
-		ActorId:      actor.Id(),
+		ActorID:      actor.Id(),
 		ActorType:    actor.Type(),
 		Role:         name,
-		ResourceId:   resource.Id(),
+		ResourceID:   resource.Id(),
 		ResourceType: resource.Type(),
 	}
-	reqBodyJson, e := json.Marshal(reqBody)
+	reqBodyJSON, e := json.Marshal(reqBody)
 	if e != nil {
 		return e
 	}
-	reqBodyBytes := bytes.NewBuffer(reqBodyJson)
+	reqBodyBytes := bytes.NewBuffer(reqBodyJSON)
 	res, e := c.post("/roles", reqBodyBytes)
+	if e != nil {
+		return e
+	}
 	defer res.Body.Close()
 	resBody, e := ioutil.ReadAll(res.Body)
 	if e != nil {
@@ -307,15 +303,15 @@ func (c Client) GetResourceRoleForActor(resource Instance, role string, actor In
 		return nil, e
 	}
 	defer res.Body.Close()
-	resBodyJson, e := ioutil.ReadAll(res.Body)
+	resBodyJSON, e := ioutil.ReadAll(res.Body)
 	if e != nil {
 		return nil, e
 	}
 	if res.StatusCode != 200 {
-		return nil, errors.New(string(resBodyJson))
+		return nil, errors.New(string(resBodyJSON))
 	}
 	var resBody []Role
-	e = json.Unmarshal(resBodyJson, &resBody)
+	e = json.Unmarshal(resBodyJSON, &resBody)
 	if e != nil {
 		return nil, e
 	}
